d4a98007ce70e0d1e63244907c1c55c49f57f37d
Doug Smith <dougsmith2015@gmail.com>
2017-04-22 02:02:39 -0400

    Added filter for urls

--- p.js
+++ p.js
@@ -3,46 +3,72 @@ var system=require('system');
 console.log('Loading a web page');
 var adr ="http://studentgovernment.nd.edu" 
 
-
 var visited = {};
-
 
 if (system.args.length > 1){
   adr=system.args[1];
 }
 
 
-
-
-
-
 //Saves the given image as a png
 function saveToImage(url, fname){
-  console.log(2);
   page=require("webpage").create();
-  //Important image-rendering feature
   page.onConsoleMessage = function(msg){
     console.log(msg);
   }
-  page.viewportSize = {
-    width: 800,
-    height: 600
-  };
-  var status; 
-  console.log(3);
-  page.open(url, function(status){
-    var file = fname;
-    if (status === "success" ){
-      window.setTimeout( (function(){
-        page.render(file);
-        phantom.exit();
-      }), 200 );  
-    }
-  });
+  //Important image-rendering feature
+  retrieve = function(){
+    page.viewportSize = {
+      width: 800,
+      height: 600
+    };
+    var status; 
+    return page.open(url, function(status){
+      var file = fname;
+      if (status === "success" ){
+        window.setTimeout( (function(){
+          page.render(file);
+          phantom.exit();
+        }), 200 );  
+      }
+    });
+  }
+
+  return retrieve();
 }
 
 
-console.log(1);
-var call = saveToImage(adr,"test.png");
+
+function search(url){
+  page=require("webpage").create();
+  page.onConsoleMessage = function(msg){
+    console.log(msg);
+  }
+
+
+  access = function(){
+    return page.open(url, function(status){
+      console.log(status);
+      if(status === "success"){
+        page.evaluate(function(path){
+          console.log(path);
+          var as = document.getElementsByTagName("a");
+          for( a in as ){
+            if( !(String(as[a]).search(path)) ){
+              console.log( String( as[a] ) );
+            }
+          }
+        },url);
+      }
+    });
+  }
+
+  access();
+}
+
+
+
+//saveToImage(adr,"test.png");
+search(adr);
 
 
