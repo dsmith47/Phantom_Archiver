9bbfceb8c9ae13c106c5fa8f6718a8cf6b814c14
Doug Smith <dougsmith2015@gmail.com>
2017-04-21 23:02:19 -0400

    wrote working image capture function

--- p.js
+++ p.js
@@ -1,75 +1,48 @@
 var system=require('system');
-var page = require('webpage').create();
-page.onConsoleMessage = function(msg){
-  console.log(msg);
-}
 
 console.log('Loading a web page');
 var adr ="http://studentgovernment.nd.edu" 
 
 
 var visited = {};
-var frontier = [adr];
 
-var search = function(url){
-  console.log( url );
-
-  page.open(url,function(status){
-    console.log( url );
-    if(status==="success"){
-      console.log("SUCCESS");
-      page.evaluate(function(){
-        console.log("Evaluating");
-        var as = document.getElementByTagName("a");
-        if( String(as[a]).search( "studentgov" ) > 0  && !visited[ as[a] ] ){
-          console.log( String(as[a]) );
-          search( String(as[a]) );
-        }
-      });
-    }
-  });
-}
 
 if (system.args.length > 1){
   adr=system.args[1];
 }
 
-while( frontier.length > 0 ){
-  url=frontier.pop();
-  search(url);
-}
 
 
 
 
 
-
-
-
-
-
-
-
-//url_search(adr, visited, page);
-
-// UTILITY FUNCTIONS ////////////
-function url_search( url,visited, page){
-  console.log(url);
-  visited[url]=true;
+//Saves the given image as a png
+function saveToImage(url, fname){
+  console.log(2);
+  page=require("webpage").create();
+  //Important image-rendering feature
+  page.onConsoleMessage = function(msg){
+    console.log(msg);
+  }
+  page.viewportSize = {
+    width: 800,
+    height: 600
+  };
+  var status; 
+  console.log(3);
   page.open(url, function(status){
-    if ( status === "success" ){
-      page.evaluate(function(visited,url,url_search){
-        var as = document.getElementsByTagName("a");
-        for (a in as){
-          if( String(as[a]).search( "studentgov" ) > 0  && !visited[ as[a] ] ){
-            url_search( String(as[a]), visited, page );
-          }
-        } 
-      }, visited,url,url_search);
-    } else {
-      console.log("FAIL");
-      phantom.exit(1);
+    var file = fname;
+    if (status === "success" ){
+      window.setTimeout( (function(){
+        page.render(file);
+        phantom.exit();
+      }), 200 );  
     }
   });
-
 }
+
+
+console.log(1);
+var call = saveToImage(adr,"test.png");
+
+
